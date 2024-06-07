115684132e377c9548a67c3e7db4237051c6e8a7
Ilya Svintsov <xeri237@gmail.com>
2024-06-07 20:00:30 +0300

    new commit

--- js/gallery.js
+++ js/gallery.js
@@ -1,12 +1,13 @@
-import { galleryItems } from "./gallery-items.js";
+import {galleryItems} from "./gallery-items.js";
 // Change code below this line
 const imagesList = document.querySelector(".gallery");
 
 imagesList.addEventListener("click", getLargeImageUrl);
+
 function makeImgMarkup(obj) {
-  return obj
-    .map((image) => {
-      return `<li class="gallery__item">
+    return obj
+        .map((image) => {
+            return `<li class="gallery__item">
     <a class="gallery__link" href="large-image.jpg">
       <img
         loading="lazy"
@@ -17,37 +18,34 @@ function makeImgMarkup(obj) {
       />
     </a>
   </li>`;
-    })
-    .join("");
+        })
+        .join("");
 }
 
 function appendImages(list) {
-  return (list.innerHTML = makeImgMarkup(galleryItems));
+    return (list.innerHTML = makeImgMarkup(galleryItems));
 }
+
 appendImages(imagesList);
 
 function getLargeImageUrl(e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  checkOnImg(e);
+    if (e.target.nodeName !== "IMG") {
+        return
+    }
 
-  const instance = basicLightbox.create(`
+    const instance = basicLightbox.create(`
   <img src="${e.target.dataset.source}">
 `);
 
-  instance.show();
+    instance.show();
 
-  document.body.addEventListener("keydown", onClose, { once: true });
+    document.body.addEventListener("keydown", onClose, {once: true});
 
-  function onClose(e) {
-    if (e.code === "Escape") {
-      instance.close();
+    function onClose(e) {
+        if (e.code === "Escape") {
+            instance.close();
+        }
     }
-  }
-
-  function checkOnImg(e) {
-    if (e.target.nodeName != "IMG") {
-      return;
-    }
-  }
 }
